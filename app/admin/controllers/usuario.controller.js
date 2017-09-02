aa4abcc3c13bf4f4f0ded8797accff9915248be2
Francisco Noé Acevedo Rivera <paconoeacevedo@gmail.com>
2017-09-02 15:59:07 -0500

    Avance admin add user

--- app/admin/controllers/usuario.controller.js
+++ app/admin/controllers/usuario.controller.js
@@ -27,11 +27,15 @@
         show();
         
         scope.agregarUsuario=agregarUsuario;
-        
-        
+        scope.verActualizarUsuario=verActualizarUsuario;
+        scope.guardarUsuario=guardarUsuario;
+        scope.eliminarUsuario=eliminarUsuario;
+        scope.cancelar=cancelar;
         
         scope.usuarios=[];
         scope.usuario={};
+        scope.nuevoUsuario=true;
+        scope.updateUsuario=false;
         
         
         
@@ -44,13 +48,44 @@
                 console.log('Error al obtener los usuarios'+error);
                 hide();
                 error();
-                
             }
         );
 
         function agregarUsuario(){
+            scope.nuevoUsuario=true;
+            scope.updateUsuario=false;
+            console.log('click en add');
+            console.log(scope.nuevoUsuario+' '+scope.updateUsuario);
             $("#modal_agregar_usuario").modal('show');
         }
+        
+        function verActualizarUsuario(usuario){
+            scope.nuevoUsuario=false;
+            scope.updateUsuario=true;
+            scope.usuario=usuario;
+            console.log(scope.usuario);
+            console.log('click en update');
+            console.log(scope.nuevoUsuario+' '+scope.updateUsuario);
+            $("#modal_agregar_usuario").modal('show');
+        }
+        
+        function guardarUsuario(){
+            alert();
+            console.log(scope.usuario);
+        }
+        
+        function eliminarUsuario(usuario){
+            
+            
+        }
+        
+        function cancelar(){
+            scope.usuario={};
+            scope.nuevoUsuario=true;
+            scope.updateUsuario=false;
+            $("#modal_agregar_usuario").modal('hide');
+        }
+        
         
     };//end controller
 
